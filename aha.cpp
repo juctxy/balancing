3a7c1c5975819aa9164d094d1bfec31921beb24a
juctxy <147407863+juctxy@users.noreply.github.com>
2025-01-03 22:50:47 +0700

    Update aha.cpp

--- aha.cpp
+++ aha.cpp
@@ -1,161 +1,157 @@
-//youtube.com/TARUNKUMARDAHAKE
-//facebook.com/TARUNKUMARDAHAKE
+#include "BluetoothSerial.h"
+#include <Arduino.h>
+BluetoothSerial serialBT;
 
-#include <PID_v1.h>
-#include <LMotorController.h>
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
-#define MIN_ABS_SPEED 20
-
-MPU6050 mpu;
-
-// MPU control/status vars
-bool dmpReady = false; // set true if DMP init was successful
-uint8_t mpuIntStatus; // holds actual interrupt status byte from MPU
-uint8_t devStatus; // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize; // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount; // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q; // [w, x, y, z] quaternion container
-VectorFloat gravity; // [x, y, z] gravity vector
-float ypr[3]; // [yaw, pitch, roll] yaw/pitch/roll container and gravity vector
-
-//PID
-double originalSetpoint = 173;
-double setpoint = originalSetpoint;
-double movingAngleOffset = 0.1;
-double input, output;
-
-//adjust these values to fit your own design
-double Kp = 60;   
-double Kd = 1.4 ;
-double Ki = 70;
-PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
-
-double motorSpeedFactorLeft = 0.6;
-double motorSpeedFactorRight = 0.6;
-//MOTOR CONTROLLER
-int ENA = 5;
-int IN1 = 6;
-int IN2 = 7;
-int IN3 = 8;
-int IN4 = 9;
-int ENB = 10;
-LMotorController motorController(ENA, IN1, IN2, ENB, IN3, IN4, motorSpeedFactorLeft, motorSpeedFactorRight);
-
-volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
-void dmpDataReady()
-{
-mpuInterrupt = true;
-}
+//Bluetooth signal Store in this variable
+char btSignal;
 
 
-void setup()
-{
-// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-Wire.begin();
-TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-Fastwire::setup(400, true);
-#endif
 
-mpu.initialize();
+//initial Speed
+int Speed = 100;
 
-devStatus = mpu.dmpInitialize();
+//declare channel for pwm Output
+#define R 0
+#define L 1
 
-// supply your own gyro offsets here, scaled for min sensitivity
-mpu.setXGyroOffset(220);
-mpu.setYGyroOffset(76);
-mpu.setZGyroOffset(-85);
-mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+//PWM Pin for Controlling the speed
+int enA = 22;
+int enB = 23;
 
-// make sure it worked (returns 0 if so)
-if (devStatus == 0)
-{
-// turn on the DMP, now that it's ready
-mpu.setDMPEnabled(true);
-
-// enable Arduino interrupt detection
-attachInterrupt(0, dmpDataReady, RISING);
-mpuIntStatus = mpu.getIntStatus();
-
-// set our DMP Ready flag so the main loop() function knows it's okay to use it
-dmpReady = true;
-
-// get expected DMP packet size for later comparison
-packetSize = mpu.dmpGetFIFOPacketSize();
-
-//setup PID
-pid.SetMode(AUTOMATIC);
-pid.SetSampleTime(10);
-pid.SetOutputLimits(-255, 255); 
-}
-else
-{
-// ERROR!
-// 1 = initial memory load failed
-// 2 = DMP configuration updates failed
-// (if it's going to break, usually the code will be 1)
-Serial.print(F("DMP Initialization failed (code "));
-Serial.print(devStatus);
-Serial.println(F(")"));
-}
-}
+//motor controlling pin
+int IN1 = 16;
+int IN2 = 17;
+int IN3 = 18;
+int IN4 = 19;
 
 
-void loop()
-{
-// if programming failed, don't try to do anything
-if (!dmpReady) return;
+void setup() {
+  Serial.begin(115200);
 
-// wait for MPU interrupt or extra packet(s) available
-while (!mpuInterrupt && fifoCount < packetSize)
-{
-//no mpu data - performing PID calculations and output to motors 
-pid.Compute();
-motorController.move(output, MIN_ABS_SPEED);
+  //Bluetooth Name
+  serialBT.begin("sibidi");
+
+  //output pin declare
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+
+  // Setup PWM channels
+  ledcSetup(R, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
+  ledcAttachPin(enA, R);
+  ledcSetup(L, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
+  ledcAttachPin(enB, L);
+
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  //Intial State of Car
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
 
 }
 
-// reset interrupt flag and get INT_STATUS byte
-mpuInterrupt = false;
-mpuIntStatus = mpu.getIntStatus();
+void loop() {
 
-// get current FIFO count
-fifoCount = mpu.getFIFOCount();
+  while (serialBT.available()) {
+    btSignal = serialBT.read();
 
-// check for overflow (this should never happen unless our code is too inefficient)
-if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-{
-// reset so we can continue cleanly
-mpu.resetFIFO();
-Serial.println(F("FIFO overflow!"));
 
-// otherwise, check for DMP data ready interrupt (this should happen frequently)
+    if (btSignal == '0') Speed = 100;
+    if (btSignal == '1') Speed = 110;
+    if (btSignal == '2') Speed = 120;
+    if (btSignal == '3') Speed = 130;
+    if (btSignal == '4') Speed = 140;
+    if (btSignal == '5') Speed = 150;
+    if (btSignal == '6') Speed = 180;
+    if (btSignal == '7') Speed = 200;
+    if (btSignal == '8') Speed = 220;
+    if (btSignal == '9') Speed = 240;
+    if (btSignal == 'q') Speed = 255;
+
+//to see the incoming signal in serial monitor
+    Serial.println(btSignal);
+    
+  //backward
+    if (btSignal == 'B') {
+      backward();
+    }
+
+  //forward
+    else if (btSignal == 'F') {
+      forward();
+    }
+
+  //LEFT
+    else if (btSignal == 'L') {
+      left();
+    }
+
+  //RIGHT
+    else if (btSignal == 'R') {
+      right();
+    }
+
+  //STOP
+    else if (btSignal == 'S') {
+      stop();
+    }
+  }
 }
-else if (mpuIntStatus & 0x02)
-{
-// wait for correct available data length, should be a VERY short wait
-while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
-// read a packet from FIFO
-mpu.getFIFOBytes(fifoBuffer, packetSize);
 
-// track FIFO count here in case there is > 1 packet available
-// (this lets us immediately read more without waiting for an interrupt)
-fifoCount -= packetSize;
+//function for control motor
+void forward() {
+  ledcWrite(R, Speed);
+  ledcWrite(L, Speed);
 
-mpu.dmpGetQuaternion(&q, fifoBuffer);
-mpu.dmpGetGravity(&gravity, &q);
-mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-input = ypr[1] * 180/M_PI + 180;
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  }
+
+void backward() {
+  ledcWrite(R, Speed);
+  ledcWrite(L, Speed);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
+
+void left() {
+  ledcWrite(R, 0);
+  ledcWrite(L, Speed);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void right() {
+  ledcWrite(R, Speed);
+  ledcWrite(L, 0);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stop() {
+    ledcWrite(R, 0);
+    ledcWrite(L, 0);
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
