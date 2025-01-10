4695ce64d89970efa30250e6a4a09338a16857a3
juctxy <147407863+juctxy@users.noreply.github.com>
2025-01-10 20:54:21 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,157 +1,127 @@
-#include "BluetoothSerial.h"
-#include <Arduino.h>
-BluetoothSerial serialBT;
+Lý Hiển Long
+#include "SoftwareSerial.h"
+SoftwareSerial HC05 (4, 3); //TX, RX
+int ENA = 7;
+int IN1 = 5;
+int IN2 = 8;
+int IN3 = 9;
+int IN4 = 10;
+int ENB = 6;
 
-//Bluetooth signal Store in this variable
-char btSignal;
-
-
-
-//initial Speed
-int Speed = 100;
-
-//declare channel for pwm Output
-#define R 0
-#define L 1
-
-//PWM Pin for Controlling the speed
-int enA = 22;
-int enB = 23;
-
-//motor controlling pin
-int IN1 = 16;
-int IN2 = 17;
-int IN3 = 18;
-int IN4 = 19;
-
+char dieu_khien;
+int Speed = 150;
 
 void setup() {
-  Serial.begin(115200);
-
-  //Bluetooth Name
-  serialBT.begin("sibidi");
-
-  //output pin declare
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-
-  // Setup PWM channels
-  ledcSetup(R, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
-  ledcAttachPin(enA, R);
-  ledcSetup(L, 5000, 8);  // Channel 0 for Motor A, 5 kHz frequency, 8-bit resolution
-  ledcAttachPin(enB, L);
-
-
+  HC05.begin(9600);
+  Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
-  //Intial State of Car
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-
-
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
 }
 
-void loop() {
-
-  while (serialBT.available()) {
-    btSignal = serialBT.read();
-
-
-    if (btSignal == '0') Speed = 100;
-    if (btSignal == '1') Speed = 110;
-    if (btSignal == '2') Speed = 120;
-    if (btSignal == '3') Speed = 130;
-    if (btSignal == '4') Speed = 140;
-    if (btSignal == '5') Speed = 150;
-    if (btSignal == '6') Speed = 180;
-    if (btSignal == '7') Speed = 200;
-    if (btSignal == '8') Speed = 220;
-    if (btSignal == '9') Speed = 240;
-    if (btSignal == 'q') Speed = 255;
-
-//to see the incoming signal in serial monitor
-    Serial.println(btSignal);
-    
-  //backward
-    if (btSignal == 'B') {
-      backward();
-    }
-
-  //forward
-    else if (btSignal == 'F') {
-      forward();
-    }
-
-  //LEFT
-    else if (btSignal == 'L') {
-      left();
-    }
-
-  //RIGHT
-    else if (btSignal == 'R') {
-      right();
-    }
-
-  //STOP
-    else if (btSignal == 'S') {
-      stop();
+void loop() 
+{
+  if (HC05.available() > 0) 
+  {
+    dieu_khien = HC05.read();
+    Serial.println(dieu_khien);
+    switch (dieu_khien) 
+    {
+      case 'F':
+        tien();
+        break;
+      case 'B':
+        lui();
+        break;
+      case 'L':
+        trai();
+        break;
+      case 'R':
+        phai();
+        break;
+      case 'I':
+        tien_phai();
+        break;
+      case 'G':
+        tien_trai();
+        break;
+      case 'J':
+        lui_phai();
+        break;
+      case 'H':
+        lui_trai();
+        break;
+      case 'S':
+        Stop();
+        break;
     }
   }
 }
 
 
-//function for control motor
-void forward() {
-  ledcWrite(R, Speed);
-  ledcWrite(L, Speed);
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  }
-
-void backward() {
-  ledcWrite(R, Speed);
-  ledcWrite(L, Speed);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-}
-
-void left() {
-  ledcWrite(R, 0);
-  ledcWrite(L, Speed);
-
+void tien() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 }
 
-void right() {
-  ledcWrite(R, Speed);
-  ledcWrite(L, 0);
-
+void lui() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 }
 
-void stop() {
-    ledcWrite(R, 0);
-    ledcWrite(L, 0);
+void phai() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
+void trai() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void tien_trai() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+}
+void tien_phai() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+}
+void lui_phai() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+}
+void lui_trai() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void Stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
